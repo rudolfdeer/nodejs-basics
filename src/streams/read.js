a776476abb02bc02da9dbd08f0b73b7a905ce68e
rudolfdeer <tomalichko@gmail.com>
2022-05-31 15:20:32 +0300

    feat: implemented streams tasks

--- src/streams/read.js
+++ src/streams/read.js
@@ -1,3 +1,11 @@
+import fs from 'fs';
+import path from 'path';
+
 export const read = async () => {
-    // Write your code here 
-};
+    const PATH = path.join(path.resolve(), './files/fileToRead.txt');
+    const readStream = fs.createReadStream(PATH);
+
+    readStream.on('data', (chunk) => {
+        process.stdout.write(chunk);
+    })
+}
